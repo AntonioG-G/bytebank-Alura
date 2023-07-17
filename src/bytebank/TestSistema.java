12232b937f018156f90ebf74c6ce304e5940a9f5
Antonio Guerrero <129303374+AntonioG-G@users.noreply.github.com>
2023-07-17 03:00:40 -0600

    Add files via upload

---
+++ src/bytebank/TestSistema.java
@@ -0,0 +1,16 @@
+package bytebank;
+
+public class TestSistema {
+
+    public static void main(String[] args) {
+        
+        SistemaInterno sistema = new SistemaInterno();
+
+        Gerente gerenteTest = new Gerente();
+
+        Administrador adminTest = new Administrador();
+
+        sistema.autentica(gerenteTest);
+        sistema.autentica(adminTest);
+    }
+}
